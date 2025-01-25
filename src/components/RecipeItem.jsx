6b143e3ff1937ad2dcf47392d824db84f51e0dab
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2025-01-25 01:33:16 +0100

    Update RecipeItem.jsx

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,44 +1,19 @@
-import { Center, Image, Text, Box } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { RecipeItem } from "./RecipeItem";
 
-export const RecipeItem = ({ recipe, onClick }) => {
+export const RecipeItems = ({ recipes, onClick }) => {
   return (
-    <Center
-      flexDirection="column"
-      gap={4}
-      cursor="pointer"
-      onClick={() => onClick(recipe)}
-      w={["90%", "45%", "30%"]} // Adjust width for responsiveness
-      mx="auto" // Centers the content
-      mb={6} // Add margin bottom to space out items
+    <Wrap
+      spacing="30px"
+      justify="center"
+      p={4}
+      wrap="wrap"
     >
-      <Image
-        src={recipe.image}
-        alt={recipe.label}
-        w="100%" // Ensure image takes full width of its container
-        h="auto" // Maintain aspect ratio
-        objectFit="cover" // Crop or scale image to fit nicely
-        borderRadius="lg"
-        border="1px solid gray"
-      />
-      <Box
-        lineHeight={1.9}
-        textAlign="center"
-        bg="gray.100"
-        p={4}
-        borderRadius="lg"
-        boxShadow="md"
-      >
-        <Text fontSize="lg" fontWeight="bold">
-          {recipe.label}
-        </Text>
-        <Text fontSize="sm" color="green.500">
-          {recipe.dietLabels.join(", ")}
-        </Text>
-        <Text fontSize="sm" color="red.500">
-          Cautions: {recipe.cautions.join(", ")}
-        </Text>
-        <Text fontSize="sm">Dish: {recipe.dishType}</Text>
-      </Box>
-    </Center>
+      {recipes.map(({ recipe }) => (
+        <WrapItem key={recipe.label}>
+          <RecipeItem recipe={recipe} onClick={onClick} />
+        </WrapItem>
+      ))}
+    </Wrap>
   );
 };
