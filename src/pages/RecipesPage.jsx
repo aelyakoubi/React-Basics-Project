6c05340ef8970aa8fe9f290c85a78151032e4048
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2025-01-25 01:20:58 +0100

    Update RecipesPage.jsx

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,9 +1,10 @@
-import { Center, Heading, Flex } from "@chakra-ui/react";
+import { Flex, Heading, Center } from "@chakra-ui/react";
 import { useState } from "react";
 import { RecipeChoice } from "../components/RecipeChoice";
 import { RecipeSearch } from "../components/RecipeSearch";
+import { RecipeItems } from "../components/RecipeItems";
 
-const RecipesPage = () => {
+const RecipesPage = ({ recipes }) => {
   const [userRecipe, setUserRecipe] = useState();
   const greeting = "Welcome to Max Recipe Checker!";
 
@@ -16,22 +17,20 @@ const RecipesPage = () => {
       width="100%"
       bg="gray.900"
       color="white"
-      px={{ base: 4, md: 8 }} // Responsive padding: smaller on mobile
-      textAlign="center" // Center text content on mobile
+      px={4}
     >
       {userRecipe ? (
         <RecipeChoice recipe={userRecipe} onClick={setUserRecipe} />
       ) : (
         <>
-          <Heading
-            size={{ base: "xl", md: "2xl" }} // Responsive font size
-            mb={{ base: 6, md: 8 }} // Responsive margin
-            color="blue.200"
-          >
+          <Heading size="2xl" mb={8} color="blue.200">
             <Center>{greeting}</Center>
           </Heading>
 
           <RecipeSearch onClick={setUserRecipe} />
+
+          {/* Use RecipeItems to render recipe cards */}
+          <RecipeItems recipes={recipes} onClick={setUserRecipe} />
         </>
       )}
     </Flex>
