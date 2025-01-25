0bce7d3897da51c88add6c79d5483457885b3c3b
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2025-01-25 01:37:08 +0100

    Update RecipeItems.jsx

--- src/components/RecipeItems.jsx
+++ src/components/RecipeItems.jsx
@@ -2,13 +2,17 @@ import { Flex } from "@chakra-ui/react";
 import { RecipeItem } from "./RecipeItem";
 
 export const RecipeItems = ({ recipes, onClick }) => {
+  if (!recipes || recipes.length === 0) {
+    return <Text>No recipes available</Text>; // Added a fallback message
+  }
+
   return (
     <Flex
       gap={16}
       w={["full", "90%"]}
       flexWrap="wrap"
       flexDir={["column", "row"]}
-      justify="even"
+      justify="center" // Center the content horizontally
       alignItems="center"
     >
       {recipes.map(({ recipe }) => (
