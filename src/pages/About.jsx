b963a372cc51685fe2cd5e98416d5467daa1e716
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2025-01-30 23:27:21 +0100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,21 @@
-import { Box, Text, VStack, Button, Center, Heading } from '@chakra-ui/react';
+import { Box, Text, VStack, Button, Center, Heading, useColorModeValue } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-
 const About = () => {
+  // Dynamic colors for Dark & Light Mode
+  const bgColor = useColorModeValue('white', 'black');
+  const textColor = useColorModeValue('black', 'white');
+  const headingColor = useColorModeValue('blue.600', 'blue.300');
+
   return (
-    <Box p={5}>
+    <Box p={5} bg={bgColor} color={textColor} minHeight="100vh">
       <Center h="100vh" flexDirection="column">
-        <Heading size="2xl" mb={5} color="blue.200">
+        <Heading size="2xl" mb={5} color={headingColor}>
           Recipe Checker
         </Heading>
         <VStack spacing={5} align="center">
           <Text fontSize="xl" textAlign="center">
-            Welcome to our Recipe Checker! Here, you can easily check and manage your recipes. Whether you a novice cook
+            Welcome to our Recipe Checker! Here, you can easily check and manage your recipes. Whether you are a novice cook
             or an experienced chef, we help you ensure your ingredients and instructions are spot on.
           </Text>
           <Text fontSize="lg" textAlign="center">
