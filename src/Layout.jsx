70cb3a37f6219439fc7fe80ea605334d6dbfd534
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2025-01-30 23:25:19 +0100

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,37 +1,15 @@
-import {
-  Box,
-  Flex,
-  useBreakpointValue,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
 const Layout = ({ children }) => {
-  // Get different padding based on screen size
-  const padding = useBreakpointValue({ base: 2, sm: 4 });
-
-  // Get background and text colors based on the color mode
-  const bgColor = useColorModeValue('gray.50', 'gray.800'); // Light and dark mode background
-  const textColor = useColorModeValue('black', 'white'); // Light and dark mode text color
+  const bgColor = useColorModeValue('white', 'black');
+  const textColor = useColorModeValue('black', 'white');
 
   return (
-    <Flex
-      direction='column'
-      width='100%'
-      minHeight='100vh'
-      bg={bgColor} // Background for the entire layout, responsive to color mode
-      color={textColor} // Text color responsive to color mode
-    >
+    <Flex direction="column" minHeight="100vh" bg={bgColor} color={textColor}>
       <Header />
-      <Box
-        as='main'
-        flex='1'
-        width='100%'
-        p={padding} // Responsive padding
-        bg='white' // Background for the main content area
-        boxShadow='sm' // Optional: Add a shadow for better separation
-      >
+      <Box as="main" flex="1" width="100%" p={4} bg={bgColor} color={textColor}>
         {children}
       </Box>
       <Footer />
